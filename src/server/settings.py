6f2f2d1125119b291a9469697f5ff6646289987a
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-07-27 15:49:48 +0300

    Default value for SECRET_KEY

--- src/server/settings.py
+++ src/server/settings.py
@@ -12,7 +12,7 @@ environ.Env.read_env(BASE_DIR.parent / '.env')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', 'insecure')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
