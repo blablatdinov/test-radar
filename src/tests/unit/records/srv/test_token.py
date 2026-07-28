a3a12492c53d32736dab9857edf2f56ae4983bae
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-07-28 23:14:34 +0300

    lint

--- src/tests/unit/records/srv/test_token.py
+++ src/tests/unit/records/srv/test_token.py
@@ -1,11 +1,10 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025-2026 Almaz Ilaetdinov <a.ilaletdinov@yandex.ru>
 # SPDX-License-Identifier: MIT
 
-import secrets
 from records.srv.token import mask_token
 
 
-def test_mask_token():
+def test_mask_token() -> None:
     got = mask_token('dev_qGhTxSaRcxv3rK6ji-pWaEjs5ATa3QJHf49ZUUm6HR4')
 
     assert got == 'dev_qG...HR4'
