cecd959c68786b5aba93402e9ec6464e2bbbd375
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2026-07-28 21:11:31 +0300

    Remove single record creating (#47)

--- src/api/urls.py
+++ src/api/urls.py
@@ -3,9 +3,8 @@
 
 from django.urls import path
 
-from api.views import BulkCreateTestRecordView, CreateTestRecordView
+from api.views import BulkCreateTestRecordView
 
 urlpatterns = [
-    path('test_record/create/', CreateTestRecordView.as_view(), name='api_create_test'),
     path('test_record/bulk_create/', BulkCreateTestRecordView.as_view(), name='api_bulk_create_test'),
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -5,39 +5,17 @@ import logging
 from http import HTTPStatus
 
 from django.db import transaction
-from rest_framework.generics import CreateAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.serializers import BaseSerializer
 from rest_framework.views import APIView
 
 from api.serializers.bulk_record import BulkCreateSerializer
-from api.serializers.record import TestRecordSerializer
-from records.models import Agent, ApiToken, TestRecord
+from records.models import Agent, TestRecord
 from records.srv.token import verify_token
 
 _TOKEN_KEYWORD = 'Token'
 
 logger = logging.getLogger('api.views')
-
-
-class CreateTestRecordView(CreateAPIView):
-    serializer_class = TestRecordSerializer
-
-    def perform_create(self, serializer: BaseSerializer) -> None:
-        agent = None
-        project = None
-        if isinstance(self.request.auth, ApiToken):
-            agent = self.request.auth.agent
-            project = agent.project
-            logger.info(
-                'Creating test record for agent %r (project=%r)',
-                agent.name,
-                project.name,
-            )
-        else:
-            logger.debug('Creating test record via session auth (user=%r)', self.request.user.username)
-        serializer.save(agent=agent, project=project)
 
 
 class BulkCreateTestRecordView(APIView):
