7d1c11294b9a9b66fa0362d2c5d0e32837d90cc0
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-07-28 09:16:45 +0300

    Fix tests

---
+++ src/conftest.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+# SPDX-License-Identifier: MIT
+
+from django.conf import settings
+
+settings.REGISTRATION_ENABLED = True
