157a60c4d62f3e0e5ca3f5fe7e9321fef4a5f30b
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-07-28 09:43:53 +0300

    Types

--- src/gui/views.py
+++ src/gui/views.py
@@ -1,11 +1,11 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025-2026 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
 # SPDX-License-Identifier: MIT
 
-from typing import Any
+from typing import Any, cast
 
 from django.views.generic import TemplateView
 
-from auth.models import User
+from auth.models import User  # noqa: TC001
 from records.models import Project
 from records.srv import record
 
@@ -18,8 +18,8 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:  # noqa: ARG002
-        user = self.request.user
-        return {'projects': user.projects.all()}
+        user = cast('User', self.request.user)
+        return {'projects': Project.objects.filter(owner=user)}
 
 
 class ProjectView(TemplateView):
@@ -28,7 +28,7 @@ class ProjectView(TemplateView):
     template_name = 'project.html'
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        user = self.request.user
+        user = cast('User', self.request.user)
         project = Project.objects.get(pk=kwargs['pk'], owner=user)
         context = record.filtered_records(project.pk, self.request)
         context['project'] = project
