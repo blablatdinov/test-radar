5620957c4e5052056cb933fd2549d2a5de3a98d5
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-07-31 23:20:57 +0300

    TODO marker

--- src/tests/it/gui/test_views.py
+++ src/tests/it/gui/test_views.py
@@ -123,6 +123,7 @@ def test_project_create_redirects_anonymous(client: Client) -> None:
 
 @pytest.mark.django_db
 @pytest.mark.skip
+# TODO: #70:30min Optimize gui.views.ProjectView
 def test_project_page_not_n_plus_one(client: Client, filled_project, django_assert_max_num_queries, user) -> None:
     client.force_login(user)
     with django_assert_max_num_queries(1):
