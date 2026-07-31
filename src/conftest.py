150dbe81af1e4edd797c47afd3567dbc0f52dda0
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2026-07-31 08:59:40 +0300

    Apply suggestion from @blablatdinov

--- src/conftest.py
+++ src/conftest.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025-2026 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
 # SPDX-License-Identifier: MIT
 
-# TODO #63 Write tests for a rendered templates
+# TODO #63:30min Write tests for a rendered templates
 
 from django.conf import settings
 
