c6efc1347197fd757bde42b6e5331f1277661f25
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-07-31 13:48:14 +0300

    Migrate to postgres

--- src/server/settings.py
+++ src/server/settings.py
@@ -83,10 +83,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
-    },
+    'default': env.db('DATABASE_URL'),
 }
 
 
