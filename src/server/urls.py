4b18a2be590f9d3a516648009a754c45934aa904
Ilaletdinov Almaz <45946541+blablatdinov@users.noreply.github.com>
2026-07-31 23:53:31 +0300

    Hide admin panel (#77)

--- src/server/urls.py
+++ src/server/urls.py
@@ -2,12 +2,13 @@
 # SPDX-License-Identifier: MIT
 
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     *debug_toolbar_urls(),
-    path('admin/', admin.site.urls),
+    path('{0}admin/'.format(settings.ADMIN_SECRET_PATH), admin.site.urls),
     path('', include('auth.urls')),
     path('', include('gui.urls')),
     path('api/v1/', include('api.urls')),
