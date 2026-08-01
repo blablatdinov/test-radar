4af9fe03a78df98a0e416a4c5368a0a10559b022
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-08-01 16:25:15 +0300

    Project not found fix

--- src/gui/views/project.py
+++ src/gui/views/project.py
@@ -5,6 +5,7 @@ from typing import Any
 
 from django.core.exceptions import PermissionDenied
 from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
 
 from records.forms import AgentForm
 from records.models import Agent, Project, TestSession
@@ -20,7 +21,7 @@ class ProjectView(TemplateView):
         if not self.request.user.is_authenticated:
             msg = 'User must be authorized.'
             raise PermissionDenied(msg)
-        project = Project.objects.get(guid=kwargs['guid'], owner=self.request.user)
+        project = get_object_or_404(Project, guid=kwargs['guid'], owner=self.request.user)
         context = record.filtered_records(project.pk, self.request)
         context['project'] = project
         context['agents'] = Agent.objects.filter(project=project).select_related('token')
