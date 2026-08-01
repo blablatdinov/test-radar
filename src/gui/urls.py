d4215ab3c4962e73d70697e6109cd487b0080314
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-08-01 13:37:46 +0300

    Fix licenses

--- src/gui/urls.py
+++ src/gui/urls.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025-2026 Almaz Ilaetdinov <a.ilaletdinov@yandex.ru>
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
 # SPDX-License-Identifier: MIT
 
 from django.urls import path
