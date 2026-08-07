57a55d9f75b2bc30dca56976e32198ee80fe24ed
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-08-07 20:36:43 +0300

    Show app version

--- src/server/context_processors.py
+++ src/server/context_processors.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025-2026 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
 # SPDX-License-Identifier: MIT
 
+import os
+
 from typing import TYPE_CHECKING
 
 from django.conf import settings
@@ -11,3 +13,7 @@ if TYPE_CHECKING:
 
 def registration_enabled(request: HttpRequest) -> dict[str, bool]:  # noqa: ARG001
     return {'registration_enabled': settings.REGISTRATION_ENABLED}
+
+
+def app_version(request: HttpRequest) -> dict[str, str]:
+    return {'app_version': os.environ.get('APP_VERSION', 'dev')}
--- src/server/settings.py
+++ src/server/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'server.context_processors.registration_enabled',
+                'server.context_processors.app_version',
             ],
         },
     },
