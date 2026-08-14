453e8839e368ba9b08419ca9b5491c636f45de42
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2026-08-14 13:55:50 +0300

    Check agent token regenerate access via membership

--- src/gui/views/agent_token_regenerate.py
+++ src/gui/views/agent_token_regenerate.py
@@ -4,11 +4,12 @@
 from typing import TYPE_CHECKING, Any, final
 
 from django.core.exceptions import PermissionDenied
+from django.http import Http404
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import View
 
 from records.models import Agent, Project
-from records.srv import token
+from records.srv import permissions, token
 
 if TYPE_CHECKING:
     import uuid
@@ -24,14 +25,13 @@ class AgentTokenRegenerateView(View):
         if not request.user.is_authenticated:
             msg = 'User must be authorized.'
             raise PermissionDenied(msg)
-        # @todo #162:30min Replace owner checks with
-        #  records/srv/permissions.can_manage_agent(user, project, agent.type)
-        #  and drop the agent.owner filter: Maintainer must be able to
-        #  regenerate tokens of any agent, Developer only of LOCAL ones.
-        #  The service keeps legacy owner behavior while
-        #  settings.RBAC_ENABLED is off.
-        project = get_object_or_404(Project, guid=guid, owner=request.user)
-        agent = get_object_or_404(Agent, guid=agent_guid, project=project, owner=request.user)
+        project = get_object_or_404(Project, guid=guid)
+        if not permissions.is_project_member(request.user, project):
+            raise Http404
+        agent = get_object_or_404(Agent, guid=agent_guid, project=project)
+        if not permissions.can_manage_agent(request.user, project, agent.type):
+            msg = 'You do not have permission to manage this agent.'
+            raise PermissionDenied(msg)
         raw_token = token.regenerate_token_for_agent(agent)
         request.session['new_token'] = raw_token
         request.session['new_agent_name'] = agent.name
